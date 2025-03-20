23ebe21b8d6c62861a50b9c7c5d3506ab3677991
arieum <lar713@naver.com>
2025-03-20 18:27:40 +0900

    refactor: 프로젝트 기본 폴더 구조 변경

--- data/build.gradle.kts
+++ data/build.gradle.kts
@@ -1,20 +1,17 @@
 plugins {
-    alias(libs.plugins.android.application)
+    alias(libs.plugins.android.library)
     alias(libs.plugins.kotlin.android)
 }
 
 android {
-    namespace = "com.binbean.binbean"
+    namespace = "com.example.data"
     compileSdk = 35
 
     defaultConfig {
-        applicationId = "com.binbean.binbean"
-        minSdk = 26
-        targetSdk = 35
-        versionCode = 1
-        versionName = "1.0"
+        minSdk = 24
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
     }
 
     buildTypes {
@@ -27,11 +24,11 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
     }
     kotlinOptions {
-        jvmTarget = "17"
+        jvmTarget = "11"
     }
 }
 
@@ -40,13 +37,9 @@ dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
-    implementation(libs.androidx.activity)
-    implementation(libs.androidx.constraintlayout)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
 
-    implementation(project(":data"))
     implementation(project(":domain"))
-    implementation(project(":presentation"))
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -21,3 +21,6 @@ dependencyResolutionManagement {
 
 rootProject.name = "BinBean"
 include(":app")
+include(":data")
+include(":presentation")
+include(":domain")
