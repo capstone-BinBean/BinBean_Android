7253da0296949be941663add8a72bc21e51ace03
arieum <lar713@naver.com>
2025-03-25 13:30:12 +0900

    feat: BinbeanApplication Hilt 적용

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
+    id("kotlin-kapt")
+    id("com.google.dagger.hilt.android")
 }
 
 android {
@@ -46,4 +48,7 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+    implementation("com.google.dagger:hilt-android:2.51.1")
+    kapt("com.google.dagger:hilt-compiler:2.51.1")
 }
---
+++ app/src/main/java/com/binbean/binbean_android/BinbeanApplication.kt
@@ -0,0 +1,9 @@
+package com.binbean.binbean_android
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class BinbeanApplication : Application() {
+
+}
