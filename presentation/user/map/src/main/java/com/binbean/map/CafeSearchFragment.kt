f585969f20929c396c54b0eae3b1ebab72b0f2ce
arieum <lar713@naver.com>
2025-04-14 15:37:50 +0900

    feat: [user_CafeSearchFragment] 카페 검색 화면 xml 구현

---
+++ presentation/user/map/src/main/java/com/binbean/map/CafeSearchFragment.kt
@@ -0,0 +1,30 @@
+package com.binbean.map
+
+import androidx.fragment.app.viewModels
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class CafeSearchFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = CafeSearchFragment()
+    }
+
+    private val viewModel: CafeSearchViewModel by viewModels()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        // TODO: Use the ViewModel
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        return inflater.inflate(R.layout.fragment_cafe_search, container, false)
+    }
+}
---
+++ presentation/user/map/src/main/java/com/binbean/map/CafeSearchViewModel.kt
@@ -0,0 +1,7 @@
+package com.binbean.map
+
+import androidx.lifecycle.ViewModel
+
+class CafeSearchViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
