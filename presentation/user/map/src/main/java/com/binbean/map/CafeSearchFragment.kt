9aa225dcc5c86cf7e0be64d700176a0481fe4a70
arieum <lar713@naver.com>
2025-05-02 22:13:41 +0900

    [feat] 카페검색 EditTextview - 카카오 로컬 api 연결

--- presentation/user/map/src/main/java/com/binbean/map/CafeSearchFragment.kt
+++ presentation/user/map/src/main/java/com/binbean/map/CafeSearchFragment.kt
@@ -2,19 +2,18 @@ package com.binbean.map
 
 import androidx.fragment.app.viewModels
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.widget.addTextChangedListener
 import com.binbean.map.databinding.FragmentCafeSearchBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class CafeSearchFragment : Fragment() {
     private lateinit var binding: FragmentCafeSearchBinding
-
-    companion object {
-        fun newInstance() = CafeSearchFragment()
-    }
-
     private val viewModel: CafeSearchViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -27,5 +26,27 @@ class CafeSearchFragment : Fragment() {
     ): View {
         binding = FragmentCafeSearchBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        setupEditTextListener()
+        observeSearchResult()
+    }
+
+    private fun setupEditTextListener() {
+        binding.searchEditText.addTextChangedListener {
+            val query = it.toString()
+            if (query.isNotEmpty()) {
+                viewModel.searchCafeKeyword(query)
+            }
+        }
+    }
+
+    private fun observeSearchResult() {
+        viewModel.searchResult.observe(viewLifecycleOwner) { result ->
+            Log.d("arieum", result.toString())
+        }
     }
 }
--- presentation/user/map/src/main/java/com/binbean/map/CafeSearchViewModel.kt
+++ presentation/user/map/src/main/java/com/binbean/map/CafeSearchViewModel.kt
@@ -1,7 +1,30 @@
 package com.binbean.map
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.binbean.domain.cafe.Cafe
+import com.binbean.domain.cafe.SearchCafeKeywordsUseCase
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
-class CafeSearchViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+@HiltViewModel
+class CafeSearchViewModel @Inject constructor(
+    private val searchCafeKeywordsUseCase: SearchCafeKeywordsUseCase
+): ViewModel() {
+    private val _searchResult = MutableLiveData<List<Cafe>>()
+    val searchResult: LiveData<List<Cafe>> = _searchResult
+
+    fun searchCafeKeyword(query: String) {
+        viewModelScope.launch {
+            try {
+                val result = searchCafeKeywordsUseCase(query)
+                _searchResult.value = result
+            } catch (e: Exception) {
+                _searchResult.value = emptyList()
+            }
+        }
+    }
 }
