d523a5fdf44316544a0991994d8ac55af4ef52df
arieum <lar713@naver.com>
2025-05-02 22:39:03 +0900

    [feat] 카페검색 리사이클러뷰 adapter 설정

--- presentation/user/map/src/main/java/com/binbean/map/CafeSearchFragment.kt
+++ presentation/user/map/src/main/java/com/binbean/map/CafeSearchFragment.kt
@@ -7,7 +7,11 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.isInvisible
+import androidx.core.view.isVisible
 import androidx.core.widget.addTextChangedListener
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.binbean.map.adapter.CafeSearchAdapter
 import com.binbean.map.databinding.FragmentCafeSearchBinding
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -15,6 +19,7 @@ import dagger.hilt.android.AndroidEntryPoint
 class CafeSearchFragment : Fragment() {
     private lateinit var binding: FragmentCafeSearchBinding
     private val viewModel: CafeSearchViewModel by viewModels()
+    private val cafeAdapter = CafeSearchAdapter()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -31,8 +36,16 @@ class CafeSearchFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        setupRecyclerView()
         setupEditTextListener()
         observeSearchResult()
+    }
+
+    private fun setupRecyclerView() {
+        binding.searchResultRecycler.apply {
+            layoutManager = LinearLayoutManager(requireContext())
+            adapter = cafeAdapter
+        }
     }
 
     private fun setupEditTextListener() {
@@ -40,13 +53,21 @@ class CafeSearchFragment : Fragment() {
             val query = it.toString()
             if (query.isNotEmpty()) {
                 viewModel.searchCafeKeyword(query)
+            } else {
+                binding.searchResultRecycler.isVisible = false
+                binding.emptyText.isInvisible = false
+                cafeAdapter.submitList(emptyList())
             }
         }
     }
 
     private fun observeSearchResult() {
         viewModel.searchResult.observe(viewLifecycleOwner) { result ->
-            Log.d("arieum", result.toString())
+            Log.d("CafeSearchFragment_EditTextView", result.toString())
+            val hasResult = result.isNotEmpty()
+            binding.searchResultRecycler.isVisible = hasResult
+            binding.emptyText.isVisible = !hasResult
+            cafeAdapter.submitList(result)
         }
     }
 }
---
+++ presentation/user/map/src/main/java/com/binbean/map/adapter/CafeSearchAdapter.kt
@@ -0,0 +1,44 @@
+package com.binbean.map.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.binbean.domain.cafe.Cafe
+import com.binbean.map.databinding.SearchCafeItemBinding
+
+class CafeSearchAdapter : ListAdapter<Cafe, CafeSearchAdapter.CafeViewHolder>(
+    DiffCallback()
+){
+
+    inner class CafeViewHolder(private val binding: SearchCafeItemBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+
+        fun bind(item: Cafe) {
+            binding.apply {
+                binding.cafeName.text = item.name
+                binding.cafeAddress.text = item.address
+            }
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CafeViewHolder {
+        val binding = SearchCafeItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return CafeViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: CafeViewHolder, position: Int) {
+        holder.bind(getItem(position))
+    }
+
+    class DiffCallback : DiffUtil.ItemCallback<Cafe>() {
+        override fun areItemsTheSame(oldItem: Cafe, newItem: Cafe): Boolean {
+            return oldItem.name == newItem.name
+        }
+
+        override fun areContentsTheSame(oldItem: Cafe, newItem: Cafe): Boolean {
+            return oldItem == newItem
+        }
+    }
+}
