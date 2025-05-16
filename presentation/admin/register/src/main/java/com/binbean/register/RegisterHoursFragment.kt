f5fa566991fe49246bfbe2d1c4dac4420820e3f2
settle54 <settle54@kangwon.ac.kr>
2025-05-16 21:08:30 +0900

    style: update changePwFragment xml margins

--- presentation/admin/register/src/main/java/com/binbean/register/RegisterHoursFragment.kt
+++ presentation/admin/register/src/main/java/com/binbean/register/RegisterHoursFragment.kt
@@ -1,12 +1,16 @@
 package com.binbean.register
 
+import android.R
+import android.app.TimePickerDialog
+import android.icu.util.Calendar
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.binbean.register.databinding.FragmentRegisterHoursBinding
+
 
 class RegisterHoursFragment : Fragment() {
     private lateinit var binding: FragmentRegisterHoursBinding
@@ -22,9 +26,30 @@ class RegisterHoursFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        setHours()
+
         binding.registerButton.setOnClickListener {
             val action = RegisterHoursFragmentDirections.actionRegistrationToDrawing()
             findNavController().navigate(action)
         }
     }
+
+    private fun setHours() {
+        binding.monStart.setOnClickListener {
+            showTimePicker()
+        }
+    }
+
+    private fun showTimePicker() {
+        val calendar = Calendar.getInstance()
+        val hour = calendar.get(Calendar.HOUR_OF_DAY)
+        val minute = calendar.get(Calendar.MINUTE)
+
+        val listener = TimePickerDialog.OnTimeSetListener { _, hourOfDay, minute ->
+            val timeString = "%02d:%02d".format(hourOfDay, minute)
+        }
+
+        TimePickerDialog(requireContext(), listener, hour, minute, true).show()
+    }
+
 }
